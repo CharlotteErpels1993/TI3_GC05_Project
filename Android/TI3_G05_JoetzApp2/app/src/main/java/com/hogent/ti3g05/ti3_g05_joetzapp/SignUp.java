4100c69dac91bbfd62488d0806c5edf7dd2b8d14
anneleenwytynck <anneleen.wytynck@hotmail.com>
2014-10-30 22:03:31 +0100

    Signup verbeterd

--- Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/SignUp.java
+++ Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/SignUp.java
@@ -188,28 +188,6 @@ public class SignUp extends Activity{
 		mConfirmPasswordEditText.setError(null);
 	}
 
-	@SuppressWarnings("deprecation")
-	public void showAlertDialog(Context context, String title, String message, Boolean status) {
-		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-
-		// Setting Dialog Title
-		alertDialog.setTitle(title);
-
-		// Setting Dialog Message
-		alertDialog.setMessage(message);
-
-		// Setting alert dialog icon
-		alertDialog.setIcon(R.drawable.fail);
-
-		// Setting OK Button
-		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-			}
-		});
-
-		// Showing Alert Message
-		alertDialog.show();
-	}
 
 	
 }
