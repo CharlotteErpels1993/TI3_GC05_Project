d653592bb912840ecbf7989a42996937453ea005
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-10-31 23:04:52 +0100

    Verwijderen categorieën

--- iOS/GC05_iOS/GC05_iOS/InloggenViewController.swift
+++ iOS/GC05_iOS/GC05_iOS/InloggenViewController.swift
@@ -2,4 +2,10 @@ class InloggenViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var wachtwoordTextField: UITextField!
     
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func gaTerugNaarInloggen(segue: UIStoryboard) {}
+    
 }
