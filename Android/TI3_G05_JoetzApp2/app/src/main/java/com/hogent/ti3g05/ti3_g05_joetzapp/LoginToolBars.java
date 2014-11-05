237952931a952cb8db90bdf99b6bd1289724561b
GillesDV <gilles.devylder@hotmail.com>
2014-11-05 16:39:56 +0100

    skeletcode van login toolbars
    layout in orde, bussiness logica & implementatie nog niet

--- Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/LoginToolBars.java
+++ Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/LoginToolBars.java
@@ -7,43 +7,36 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LoginToolBars.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LoginToolBars#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoginToolBars extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //private String mParam1;
+    //private String mParam2;
+    private Button btnLogin;
+    private Button btnLogout;
+    private Button btnRegister;
+    private TextView txtCurrentUser;
+
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginToolBars.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static LoginToolBars newInstance(String param1, String param2) {
         LoginToolBars fragment = new LoginToolBars();
-        Bundle args = new Bundle();
+        /*Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        fragment.setArguments(args);*/
         return fragment;
     }
 
@@ -54,10 +47,14 @@ public class LoginToolBars extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+
+        //btnLogin = (Button) findViewById(R.id.btn_login);
+
+
+        /*if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        }*/
     }
 
     @Override
@@ -104,6 +101,30 @@ public class LoginToolBars extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onResume(){ //alle code wordt in onResume gestoken, want op dit punt is de fragment zeker al verbonden met de acvtivity.
+        super.onResume();
+
+        if (ParseUser.getCurrentUser() == null){
+            //nog niet ingelogd
+
+
+        }
+        else{
+            //gebruiker is ingelogd
+
+
+        }
+    }
+
+    public void loginButtonWillBecomeVisible(){
+
+    }
+
+    public void logoutButtonWillBecomevisible(){
+
     }
 
 }
