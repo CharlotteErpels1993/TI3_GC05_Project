ac497d0453790854312d930f67afc506740f2b27
GillesDV <gilles.devylder@hotmail.com>
2014-11-09 11:45:45 +0100

    layout inschrijven vakantie is ingevuld

---
+++ Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/InschrijvenVakantiePart1.java
@@ -0,0 +1,51 @@
+package com.hogent.ti3g05.ti3_g05_joetzapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class InschrijvenVakantiePart1 extends FragmentActivity {
+
+    //!!!!!!!!!!!!!!!!!!!!!!!
+    //voor date picker:
+    //http://developer.android.com/guide/topics/ui/controls/pickers.html#DatePicker
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inschrijven_vakantie_part1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_inschrijven_vakantie_part1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new CustomDatePicker();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+}
