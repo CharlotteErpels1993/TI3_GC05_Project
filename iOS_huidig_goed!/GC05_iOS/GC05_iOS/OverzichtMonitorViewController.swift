086e88c3653bde7ee0da70334a05087ec26a0c92
Steefanieee <Steefanieee@MacBook-Pro-van-Stefanie.local>
2014-11-14 12:17:51 +0100

    Back buttons + naam monitor overzicht

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/OverzichtMonitorViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/OverzichtMonitorViewController.swift
@@ -2,11 +2,14 @@ import UIKit
 
 class OverzichtMonitorViewController: UIViewController
 {
-    var monitor: Monitor?
+    var monitor: Monitor!
+    
+    @IBOutlet weak var welkomTxt: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true)
+        welkomTxt.text = "Welkom \(String(monitor.voornaam!)) \(String(monitor.naam!)),"
     }
     
     @IBAction func gaTerugNaarOverzichtMonitor(segue: UIStoryboardSegue) {
