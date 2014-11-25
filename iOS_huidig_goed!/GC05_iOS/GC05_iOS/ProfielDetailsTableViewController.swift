4efba77880fe637830513c6334751216631bbfd1
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-11-25 19:05:58 +0100

    Profielen worden getoond + details
    Profielen worden alfabetisch getoond

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/ProfielDetailsTableViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/ProfielDetailsTableViewController.swift
@@ -1,9 +1,28 @@
 import UIKit
 
 class ProfielDetailsTableViewController: UITableViewController {
+    @IBOutlet weak var voornaamLabel: UILabel!
+    @IBOutlet weak var naamLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var telefoonLabel: UILabel!
+    @IBOutlet weak var gsmLabel: UILabel!
+    @IBOutlet weak var facebookLabel: UILabel!
+    
+    var monitor: Monitor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //na implementatie van deze klasse moet activityIndicatorView nog bij worden opgeroepen!
+        hideSideMenuView()
+        
+        voornaamLabel.text = "Voornaam: \(monitor.voornaam!)"
+        naamLabel.text = "Naam: \(monitor.naam!)"
+        emailLabel.text = "Email: \(monitor.email!)"
+        telefoonLabel.text = "Telefoon: \(monitor.telefoon!)"
+        gsmLabel.text = "Gsm: \(monitor.gsm!)"
+        facebookLabel.text = "Facebook: \(monitor.linkFacebook!)"
+        
     }
+    
+    
 }
