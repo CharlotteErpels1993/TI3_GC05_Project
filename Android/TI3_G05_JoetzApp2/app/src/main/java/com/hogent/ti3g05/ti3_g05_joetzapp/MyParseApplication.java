fc6e36abd375eb985f7332db2dda6c748a6c34c5
anneleenwytynck <anneleen.wytynck@hotmail.com>
2014-11-25 16:20:59 +0100

    User wordt uitgelogged bij start app

--- Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/MyParseApplication.java
+++ Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/MyParseApplication.java
@@ -3,6 +3,7 @@ package com.hogent.ti3g05.ti3_g05_joetzapp;
 import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MyParseApplication extends Application {
     @Override
@@ -10,6 +11,10 @@ public class MyParseApplication extends Application {
         super.onCreate();
         Parse.initialize(this, "a3jgklEb2rHZYcgqDezLfqSP6i1C2u4eVV8R03YS", "3ZguW3kx5J6PuieccT7ypJ5ZvYhwX08ESKL8cDNX");
 
+        if(ParseUser.getCurrentUser()!= null)
+        {
+            ParseUser.logOut();
+        }
         //Parse.enableLocalDatastore(this);
 
         //ParseObject testObject = new ParseObject("TestObject");
