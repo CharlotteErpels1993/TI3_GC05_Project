b9be745c3d043713f510c06c917cca43d791643d
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-11-27 10:32:58 +0100

    Indienen voorkeur toon periode
    Gebruiker kan geen periodes meer ingeven

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/IndienenVoorkeurViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/IndienenVoorkeurViewController.swift
@@ -4,7 +4,8 @@ class IndienenVoorkeurViewController: UIViewController, UIPickerViewDataSource, 
     
     @IBOutlet weak var pickerView: UIPickerView!
     
-    @IBOutlet weak var txtViewPeriodes: UITextView!
+    //@IBOutlet weak var txtViewPeriodes: UITextView!
+    @IBOutlet weak var periodeLabel: UILabel!
     
     var vakanties: [Vakantie] = []
     var pickerData: [String] = []
@@ -15,40 +16,19 @@ class IndienenVoorkeurViewController: UIViewController, UIPickerViewDataSource, 
     @IBAction func gaTerugNaarOverzicht(sender: AnyObject) {
         annuleerControllerVoorkeur(self)
     }
-    
-    /*func zoekVakanties() {
-        var query = PFQuery(className: "Vakantie")
-        query.findObjectsInBackgroundWithBlock({(NSArray objects, NSError error) in
-            if(error == nil) {
-                if let PFObjects = objects as? [PFObject!] {
-                    for object in PFObjects {
-                        var vakantie = Vakantie(vakantie: object)
-                        self.vakanties.append(vakantie)
-                        var titel = vakantie.titel
-                        self.pickerData.append(titel!)
-                        self.pickerView.reloadAllComponents()
-                    }
-                    
-                }
-                
-                
-            }
-            
-        })
-    }*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
         hideSideMenuView()
+        periodeLabel.hidden = true
         
-        //zoekVakanties()
         vakanties = ParseData.getAlleVakanties()
         
         for vakantie in vakanties {
             pickerData.append(vakantie.titel!)
         }
         
-        giveUITextViewDefaultBorder(txtViewPeriodes)
+        //giveUITextViewDefaultBorder(txtViewPeriodes)
         
         pickerView.delegate = self
         pickerView.dataSource = self
@@ -65,7 +45,24 @@ class IndienenVoorkeurViewController: UIViewController, UIPickerViewDataSource, 
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        periodeLabel.hidden = true
+        /*var vertrekdatumStr = self.voorkeur.vakantie?.vertrekdatum.toS("dd/MM/yyyy")
+        var terugkeerdatumStr = self.voorkeur.vakantie?.terugkeerdatum.toS("dd/MM/yyyy")
+        periodeLabel.text = ("\(vertrekdatumStr) \(terugkeerdatumStr)")*/
         return pickerData[row]
+    }
+    @IBAction func toonPeriode(sender: AnyObject) {
+        var vertrekdatumStr = self.voorkeur.vakantie?.vertrekdatum.toS("dd/MM/yyyy")
+        var terugkeerdatumStr = self.voorkeur.vakantie?.terugkeerdatum.toS("dd/MM/yyyy")
+        
+        if self.voorkeur.vakantie == nil {
+            var vakantie = vakanties[0]
+            vertrekdatumStr = vakantie.vertrekdatum.toS("dd/MM/yyyy")
+            terugkeerdatumStr = vakantie.terugkeerdatum.toS("dd/MM/yyyy")
+        }
+        
+        periodeLabel.text = ("\(vertrekdatumStr!) - \(terugkeerdatumStr!)")
+        periodeLabel.hidden = false
     }
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -85,11 +82,11 @@ class IndienenVoorkeurViewController: UIViewController, UIPickerViewDataSource, 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let indienenVoorkeurSuccesvolViewController = segue.destinationViewController as IndienenVoorkeurSuccesvolViewController
             
-        if txtViewPeriodes.text.isEmpty {
+        /*if txtViewPeriodes.text.isEmpty {
             giveUITextViewRedBorder(txtViewPeriodes)
             foutBoxOproepen("Fout", "Gelieve het veld in te vullen!", self)
             
-        } else {
+        } else {*/
             
             var query = PFQuery(className: "Monitor")
             query.whereKey("email", containsString: PFUser.currentUser().email)
@@ -97,16 +94,18 @@ class IndienenVoorkeurViewController: UIViewController, UIPickerViewDataSource, 
             var monitor = Monitor(monitor: monitorPF)
             
             self.voorkeur.monitor = monitor
-            self.voorkeur.data = txtViewPeriodes.text
+            //self.voorkeur.data = txtViewPeriodes.text
             
             if self.voorkeur.vakantie == nil {
                 self.voorkeur.vakantie = vakanties[0]
             }
             
             indienenVoorkeurSuccesvolViewController.voorkeur = self.voorkeur
-        }
+        //}
 
     }
+    
+    
 
     
     @IBAction func toggle(sender: AnyObject) {
