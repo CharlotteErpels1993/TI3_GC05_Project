594ee6ca067c87bfffd124a24754109bec55df4b
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-12-02 08:30:11 +0100

    CollectionView update

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/AfbeeldingCollectionViewCell.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/AfbeeldingCollectionViewCell.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class AfbeeldingCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var image: UIImageView!
+    /*@IBOutlet weak var image: UIImageView!
     
     
     override init(frame: CGRect) {
@@ -11,5 +11,26 @@ class AfbeeldingCollectionViewCell: UICollectionViewCell {
     
     required init(coder aDecorder: NSCoder) {
         super.init(coder: aDecorder)
+    }*/
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    //let textLabel: UILabel!
+    let imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 16, width: frame.size.width, height: frame.size.height*2/3))
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        contentView.addSubview(imageView)
+        
+        //let textFrame = CGRect(x: 0, y: 32, width: frame.size.width, height: frame.size.height/3)
+        //textLabel = UILabel(frame: textFrame)
+        //textLabel.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
+        //textLabel.textAlignment = .Center
+        //contentView.addSubview(textLabel)
     }
 }
--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/AfbeeldingenViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/AfbeeldingenViewController.swift
@@ -1,29 +1,45 @@
 import UIKit
 
-class AfbeeldingenViewController: UICollectionViewController {
+class AfbeeldingenViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView: UICollectionView?
     var images: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 90)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(AfbeeldingCollectionViewCell.self, forCellWithReuseIdentifier: "afbeeldingcell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "afbeelding" {
             let afbeeldingViewController = segue.destinationViewController as AfbeeldingDetailViewController
-            var indexPath: NSIndexPath = self.collectionView.indexPathsForSelectedItems()?.last! as NSIndexPath
+            var indexPath: NSIndexPath = collectionView?.indexPathsForSelectedItems()?.last as NSIndexPath
             var image: UIImage! = self.images[indexPath.row]
             afbeeldingViewController.image = image
         }
     }
-    
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
     
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("afbeeldingcell", forIndexPath: indexPath) as AfbeeldingCollectionViewCell
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("afbeeldingcell", forIndexPath: indexPath) as AfbeeldingCollectionViewCell
         let image = images[indexPath.row]
-        cell.image.image = image
+        cell.imageView?.image = image
         return cell
     }
 }
