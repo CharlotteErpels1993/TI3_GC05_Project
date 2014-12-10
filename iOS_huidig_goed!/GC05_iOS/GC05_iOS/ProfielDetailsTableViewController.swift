04206b9ef99ecfa5266ae41ff1eaf82f49e4bd6b
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-12-10 18:55:22 +0100

    Andere gegevens monitor toegevoegd

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/ProfielDetailsTableViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/ProfielDetailsTableViewController.swift
@@ -5,6 +5,11 @@ class ProfielDetailsTableViewController: UITableViewController, MFMailComposeVie
     @IBOutlet weak var voornaamLabel: UILabel!
     @IBOutlet weak var naamLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet var straatLabel: UILabel!
+    @IBOutlet var nummerLabel: UILabel!
+    @IBOutlet var busLabel: UILabel!
+    @IBOutlet var postcodeLabel: UILabel!
+    @IBOutlet var gemeenteLabel: UILabel!
     @IBOutlet weak var telefoonLabel: UILabel!
     @IBOutlet weak var gsmLabel: UILabel!
     
@@ -19,6 +24,17 @@ class ProfielDetailsTableViewController: UITableViewController, MFMailComposeVie
         voornaamLabel.text = monitor.voornaam!
         naamLabel.text = monitor.naam!
         emailLabel.text = monitor.email!
+        straatLabel.text = monitor.straat!
+        nummerLabel.text = String(monitor.nummer!)
+        
+        if monitor.bus == "" {
+            busLabel.text = " "
+        } else {
+            busLabel.text = monitor.bus
+        }
+        
+        postcodeLabel.text = String(monitor.postcode!)
+        gemeenteLabel.text = monitor.gemeente!
         
         if monitor.telefoon == "" {
             telefoonLabel.text = "Niet beschikbaar"
