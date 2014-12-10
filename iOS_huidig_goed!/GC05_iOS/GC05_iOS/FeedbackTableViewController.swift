24e2c066f601fbc7631b75ca826314c7d63b78e3
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-12-10 15:18:06 +0100

    Feedback layout aangepast

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/FeedbackTableViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/FeedbackTableViewController.swift
@@ -16,8 +16,9 @@ class FeedbackTableViewController: UITableViewController, UISearchBarDelegate, U
     override func viewDidLoad() {
         super.viewDidLoad()
         hideSideMenuView()
-        tableView.estimatedRowHeight = 44.0
-        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        self.tableView.estimatedRowHeight = 44.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
         
         self.setNeedsStatusBarAppearanceUpdate()
         self.navigationController!.toolbarHidden = true
