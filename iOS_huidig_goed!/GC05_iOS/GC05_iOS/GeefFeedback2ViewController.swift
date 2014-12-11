88c7d54e15bf12f798429aa8f78cd9c6896987de
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-12-11 17:05:01 +0100

    Feedback wordt getoond met sterren

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/GeefFeedback2ViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/GeefFeedback2ViewController.swift
@@ -1,15 +1,22 @@
 import UIKit
 
-class GeefFeedback2ViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class GeefFeedback2ViewController: UITableViewController/*, UIPickerViewDataSource, UIPickerViewDelegate*/ {
     
-    @IBOutlet var scorePickerView: UIPickerView!
+   // @IBOutlet var scorePickerView: UIPickerView!
     @IBOutlet var txtFeedback: UITextView!
+    @IBOutlet weak var ster1: UIButton!
+    @IBOutlet weak var ster2: UIButton!
+    @IBOutlet weak var ster3: UIButton!
+    @IBOutlet weak var ster4: UIButton!
+    @IBOutlet weak var ster5: UIButton!
     
-    var scores: [Int] = [1, 2, 3, 4, 5]
+    //var scores: [Int] = [1, 2, 3, 4, 5]
     var score: Int?
     var feedback: Feedback!
     var vakantie: Vakantie!
     var titel: String?
+    var legeSter: UIImage = UIImage(named: "star2")!
+    var volleSter: UIImage = UIImage(named: "star")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +29,12 @@ class GeefFeedback2ViewController: UITableViewController, UIPickerViewDataSource
         txtFeedback.layer.borderWidth = 1.0
         txtFeedback.layer.cornerRadius = 5.0
         
-        scorePickerView.delegate = self
-        scorePickerView.dataSource = self
-        scorePickerView.reloadAllComponents()
+        //scorePickerView.delegate = self
+        //scorePickerView.dataSource = self
+        //scorePickerView.reloadAllComponents()
     }
     
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    /*func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
     }
     
@@ -41,7 +48,56 @@ class GeefFeedback2ViewController: UITableViewController, UIPickerViewDataSource
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
             self.score = scores[row]
+    }*/
+    
+    @IBAction func klikopSter1(sender: AnyObject) {
+        ster1.setImage(volleSter, forState: UIControlState.Normal)
+        ster2.setImage(legeSter, forState: UIControlState.Normal)
+        ster3.setImage(legeSter, forState: UIControlState.Normal)
+        ster4.setImage(legeSter, forState: UIControlState.Normal)
+        ster5.setImage(legeSter, forState: UIControlState.Normal)
+        self.score = 1
     }
+    
+    @IBAction func klikopSter2(sender: AnyObject) {
+        ster1.setImage(volleSter, forState: UIControlState.Normal)
+        ster2.setImage(volleSter, forState: UIControlState.Normal)
+        ster3.setImage(legeSter, forState: UIControlState.Normal)
+        ster4.setImage(legeSter, forState: UIControlState.Normal)
+        ster5.setImage(legeSter, forState: UIControlState.Normal)
+        self.score = 2
+    }
+    
+    @IBAction func klikopSter3(sender: AnyObject) {
+        ster1.setImage(volleSter, forState: UIControlState.Normal)
+        ster2.setImage(volleSter, forState: UIControlState.Normal)
+        ster3.setImage(volleSter, forState: UIControlState.Normal)
+        ster4.setImage(legeSter, forState: UIControlState.Normal)
+        ster5.setImage(legeSter, forState: UIControlState.Normal)
+        self.score = 3
+    }
+    
+    
+    @IBAction func klikopSter4(sender: AnyObject) {
+        ster1.setImage(volleSter, forState: UIControlState.Normal)
+        ster2.setImage(volleSter, forState: UIControlState.Normal)
+        ster3.setImage(volleSter, forState: UIControlState.Normal)
+        ster4.setImage(volleSter, forState: UIControlState.Normal)
+        ster5.setImage(legeSter, forState: UIControlState.Normal)
+        self.score = 4
+    }
+    
+    @IBAction func klikopSter5(sender: AnyObject) {
+        ster1.setImage(volleSter, forState: UIControlState.Normal)
+        ster2.setImage(volleSter, forState: UIControlState.Normal)
+        ster3.setImage(volleSter, forState: UIControlState.Normal)
+        ster4.setImage(volleSter, forState: UIControlState.Normal)
+        ster5.setImage(volleSter, forState: UIControlState.Normal)
+        self.score = 5
+    }
+    
+    
+    
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 2
@@ -59,9 +115,6 @@ class GeefFeedback2ViewController: UITableViewController, UIPickerViewDataSource
         feedback.datum = NSDate()
         feedback.gebruiker = gebruiker
         feedback.vakantie = self.vakantie
-        if self.score == nil {
-            self.score = scores[0]
-        }
         feedback.score = self.score
         feedback.waardering = self.txtFeedback.text
         feedback.goedgekeurd = false
