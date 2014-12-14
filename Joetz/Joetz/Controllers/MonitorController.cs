82f7dbc3de894b2fb986e2b30365cc08715d72f9
GillesDV <gilles.devylder@hotmail.com>
2014-12-14 16:48:26 +0100

    update op monitoren maken
    layout is al ietwat goed. Geen error meer. backend moet nog in orde

--- Joetz/Joetz/Controllers/MonitorController.cs
+++ Joetz/Joetz/Controllers/MonitorController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using System.Web;
+using System.IO;
 using Joetz.Models.DAL;
 using Joetz.Models.Domain;
 
@@ -20,11 +22,33 @@ namespace Joetz.Controllers
             this.monitorRepository = monitorRepository;
         }
 
-        public async Task<ActionResult> Index()
+        /*public async Task<ActionResult> Index()
         {
             var monitorenTask = monitorRepository.FindAll();
             IEnumerable<Monitor> monitoren = await monitorenTask;
             return View(monitoren);
+        }*/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //onderstaande methode dient voor de fileupload
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads/"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Index");
         }
 
         //public ActionResult Create()
