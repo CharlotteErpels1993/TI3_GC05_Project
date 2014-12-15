651cc3fb84b7329460b09d65f2b88f2a431b9e0b
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-12-15 09:47:53 +0100

    Vormingen = dynamic !

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/VormingDetailsTableViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/VormingDetailsTableViewController.swift
@@ -7,7 +7,7 @@ class VormingDetailsTableViewController: UITableViewController {
     var euro: String = "€"
     var fetchedResultController: NSFetchedResultsController = NSFetchedResultsController()
     
-    @IBOutlet weak var korteBeschrijvingLabel: UILabel!
+    /*@IBOutlet weak var korteBeschrijvingLabel: UILabel!
     @IBOutlet weak var criteriaDeelnemersLabel: UILabel!
     @IBOutlet weak var locatieLabel: UILabel!
     @IBOutlet weak var websiteLocatieLabel: UILabel!
@@ -15,7 +15,7 @@ class VormingDetailsTableViewController: UITableViewController {
     @IBOutlet weak var betalingsWijzeLabel: UILabel!
     @IBOutlet weak var inbegrepenPrijsLabel: UILabel!
     @IBOutlet weak var periodesLabel: UILabel!
-    @IBOutlet weak var tipsLabel: UILabel!
+    @IBOutlet weak var tipsLabel: UILabel!*/
     
     @IBAction func toggle(sender: AnyObject) {
         toggleSideMenuView()
@@ -24,17 +24,19 @@ class VormingDetailsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         hideSideMenuView()
+        self.tableView.estimatedRowHeight = 44.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
         
         navigationItem.title = vorming.titel
         
-        korteBeschrijvingLabel.text = vorming.korteBeschrijving
+        /*korteBeschrijvingLabel.text = vorming.korteBeschrijving
         criteriaDeelnemersLabel.text = vorming.criteriaDeelnemers
         locatieLabel.text = String("Locatie: \(vorming.locatie!)")
         websiteLocatieLabel.text = ("Website locatie: \(vorming.websiteLocatie!)")
         prijsLabel.text = String("Prijs: \(vorming.prijs!) " + euro)
         betalingsWijzeLabel.text = String("Betalingswijze: \(vorming.betalingWijze!)")
-        inbegrepenPrijsLabel.text = String("Inbegrepen in de prijs: \(vorming.inbegrepenPrijs!)")
-        tipsLabel.text = vorming.tips
+        inbegrepenPrijsLabel.text = String("Inbegrepen in de prijs: \(vorming.inbegrepenPrijs!)")*/
+        //tipsLabel.text = vorming.tips
         
         var gebruikerPF = PFUser.currentUser()
         var soort: String = gebruikerPF["soort"] as String
@@ -62,18 +64,85 @@ class VormingDetailsTableViewController: UITableViewController {
             let extraTekstViewController = segue.destinationViewController as ExtraTekstViewController
             extraTekstViewController.tekst = self.vorming.criteriaDeelnemers!
             extraTekstViewController.type = 3
-        } else if segue.identifier == "betalingsWijzeVorming" {
+        } else if segue.identifier == "betalingswijzeVorming" {
             let extraTekstViewController = segue.destinationViewController as ExtraTekstViewController
             extraTekstViewController.tekst = self.vorming.betalingWijze!
             extraTekstViewController.type = 4
-        } else if segue.identifier == "inbegrepenPrijs" {
+        } else if segue.identifier == "tipsVorming" {
+            let extraTekstViewController = segue.destinationViewController as ExtraTekstViewController
+            extraTekstViewController.tekst = self.vorming.tips!
+            extraTekstViewController.type = 5
+        }
+        /*} else if segue.identifier == "inbegrepenPrijs" {
             let extraTekstViewController = segue.destinationViewController as ExtraTekstViewController
             extraTekstViewController.tekst = self.vorming.inbegrepenPrijs!
             extraTekstViewController.type = 2
-        } else if segue.identifier == "periodes" {
+        }*/
+        /*} else if segue.identifier == "periodes" {
             let extraTekstViewController = segue.destinationViewController as ExtraTekstViewController
             extraTekstViewController.tekst = vorming.periodesToString(vorming.periodes!)
             extraTekstViewController.type = 5
+        }*/
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 9
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell: UITableViewCell!
+        
+        if indexPath.section == 0 {
+            cell = tableView.dequeueReusableCellWithIdentifier("KorteBeschrijving", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = vorming.korteBeschrijving
+        } else if indexPath.section == 1 {
+           cell = tableView.dequeueReusableCellWithIdentifier("Criteria", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = vorming.criteriaDeelnemers
+        } else if indexPath.section == 2 {
+           cell = tableView.dequeueReusableCellWithIdentifier("Locatie", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = String("Locatie: \(vorming.locatie!)")
+        } else if indexPath.section == 3 {
+            cell = tableView.dequeueReusableCellWithIdentifier("WebsiteLocatie", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = String("Website: \(vorming.websiteLocatie!)")
+        } else if indexPath.section == 4 {
+            cell = tableView.dequeueReusableCellWithIdentifier("Prijs", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = String("Prijs: \(vorming.prijs!)" + euro)
+        } else if indexPath.section == 5 {
+            cell = tableView.dequeueReusableCellWithIdentifier("Betalingswijze", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = String("Betalingswijze: \(vorming.betalingWijze!)")
+        } else if indexPath.section == 6 {
+            cell = tableView.dequeueReusableCellWithIdentifier("InbegrepenPrijs", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = String("Inbegrepen in de prijs: \(vorming.inbegrepenPrijs!)")
+        } else if indexPath.section == 7 {
+            cell = tableView.dequeueReusableCellWithIdentifier("Periodes", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = vorming.periodesToString(vorming.periodes!)
+        } else if indexPath.section == 8 {
+            cell = tableView.dequeueReusableCellWithIdentifier("Tips", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = vorming.tips!
         }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return ("Korte beschrijving")
+        } else if section == 1 {
+            return ("Criteria")
+        } else if section == 2 {
+            return ("Locatie")
+        } else if section == 4 {
+            return ("Prijs")
+        } else if section == 7 {
+            return ("Periodes")
+        } else if section == 8 {
+            return ("Tips")
+        }
+        return ("")
     }
 }
