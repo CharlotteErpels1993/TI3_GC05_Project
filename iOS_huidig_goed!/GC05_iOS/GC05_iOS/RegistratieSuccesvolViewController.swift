39908164f43b2e398a1b5b61df2af7e3ad13a5c3
Steefanieee <stefanie_geldof1994@hotmail.com>
2014-12-16 17:07:32 +0100

    RegistratieSuccesvol documentatie toegevoegd

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/RegistratieSuccesvolViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/RegistratieSuccesvolViewController.swift
@@ -5,6 +5,17 @@ class RegistratieSuccesvolViewController: UIViewController
     var ouder: Ouder!
     var wachtwoordOuder: String!
     
+    //
+    //Naam: gaTerugNaarInloggen
+    //
+    //Werking: - zorgt voor een unwind segue (gaat naar overzicht vakanties)
+    //         - zorgt er ook voor dat bij het binnenkomen bij vakanties de side bar menu verborgen is
+    //
+    //Parameters:
+    //  - sender: AnyObject
+    //
+    //Return:
+    //
     @IBAction func gaTerugNaarInloggen(sender: AnyObject) {
         let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         var destViewController = mainStoryboard.instantiateViewControllerWithIdentifier("Vakanties") as UIViewController
@@ -12,6 +23,16 @@ class RegistratieSuccesvolViewController: UIViewController
         hideSideMenuView()
     }
     
+    //
+    //Naam: viewDidLoad
+    //
+    //Werking: - schrijft de geregistreerde ouder naar de databank
+    //         - zorgt ervoor dat de gebruiker niet meer kan terugkeren, door het verbergen van de hide bar button
+    //
+    //Parameters:
+    //
+    //Return:
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         ParseToDatabase.parseOuderToDatabase(ouder, wachtwoord: wachtwoordOuder)
