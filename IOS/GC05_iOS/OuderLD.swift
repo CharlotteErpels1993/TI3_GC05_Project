43f45e505d41157d00a1729797967fb4f59868b8
CharlotteErpels1993 <charlotte.erpels@hotmail.com>
2015-02-25 14:08:46 +0100

    Local Datastore klassen afgewerkt

---
+++ IOS/GC05_iOS/OuderLD.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+struct OuderLD {
+    
+    //
+    //Function: getOuders
+    //
+    //Deze functie zet een array van PFObject om naar een array van Ouder.
+    //
+    //Parameters: - objecten: [PFObject]
+    //
+    //Return: een array van Ouder
+    //
+    static func getOuders(objecten: [PFObject]) -> [Ouder] {
+        var ouders: [Ouder] = []
+        
+        for object in objecten {
+            ouders.append(getOuder(object))
+        }
+        
+        return ouders
+    }
+    
+    //
+    //Function: getOuder
+    //
+    //Deze functie zet een  PFObject om naar een Ouder.
+    //
+    //Parameters: - object: PFObject
+    //
+    //Return: een Ouder
+    //
+    static private func getOuder(object: PFObject) -> Ouder {
+        var ouder: Ouder = Ouder(id: object.objectId)
+        
+        ouder.rijksregisterNr = object[Constanten.COLUMN_RIJKSREGISTERNUMMER] as? String
+        ouder.email = object[Constanten.COLUMN_EMAIL] as? String
+        ouder.voornaam = object[Constanten.COLUMN_VOORNAAM] as? String
+        ouder.naam = object[Constanten.COLUMN_NAAM] as? String
+        ouder.straat = object[Constanten.COLUMN_STRAAT] as? String
+        ouder.nummer = object[Constanten.COLUMN_NUMMER] as? Int
+        ouder.postcode = object[Constanten.COLUMN_POSTCODE] as? Int
+        ouder.gemeente = object[Constanten.COLUMN_GEMEENTE] as? String
+        ouder.gsm = object[Constanten.COLUMN_GSM] as? String
+        
+        if object[Constanten.COLUMN_BUS] != nil {
+            ouder.bus = object[Constanten.COLUMN_BUS] as? String
+        } else {
+            ouder.bus = ""
+        }
+        
+        if object[Constanten.COLUMN_TELEFOON] != nil {
+            ouder.telefoon = object[Constanten.COLUMN_TELEFOON] as? String
+        } else {
+            ouder.telefoon = ""
+        }
+        
+        if object[Constanten.COLUMN_AANSLUITINGSNUMMER] != nil {
+            ouder.aansluitingsNr = object[Constanten.COLUMN_AANSLUITINGSNUMMER] as? Int
+        } else {
+            ouder.aansluitingsNr = 0
+        }
+        
+        if object[Constanten.COLUMN_CODEGERECHTIGDE] != nil {
+            ouder.codeGerechtigde = object[Constanten.COLUMN_CODEGERECHTIGDE] as? Int
+        } else {
+            ouder.codeGerechtigde = 0
+        }
+        
+        if object[Constanten.COLUMN_CODEGERECHTIGDE] != nil {
+            ouder.aansluitingsNrTweedeOuder = object[Constanten.COLUMN_AANSLUITINGSNUMMERTWEEDEOUDER] as? Int
+        } else {
+            ouder.aansluitingsNrTweedeOuder = 0
+        }
+        
+        return ouder
+    }
+}
---
+++ IOS/GC05_iOS/VormingLD.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+struct VormingLD {
+    
+    //
+    //Function: getVormingen
+    //
+    //Deze functie zet een array van PFObject om naar een array van Vorming.
+    //
+    //Parameters: - objecten: [PFObject]
+    //
+    //Return: een array van Vorming
+    //
+    static func getVorming(objecten: [PFObject]) -> [Vorming] {
+        var vormingen: [Vorming] = []
+        
+        for object in objecten {
+            vormingen.append(getVorming(object))
+        }
+        
+        return vormingen
+    }
+    
+    //
+    //Function: getVorming
+    //
+    //Deze functie zet een  PFObject om naar een Vorming.
+    //
+    //Parameters: - object: PFObject
+    //
+    //Return: een Vorming
+    //
+    static private func getVorming(object: PFObject) -> Vorming {
+        var vorming: Vorming = Vorming(id: object.objectId)
+        
+        vorming.titel = object[Constanten.COLUMN_TITEL] as? String
+        vorming.locatie = object[Constanten.COLUMN_LOCATIE] as? String
+        vorming.korteBeschrijving = object[Constanten.COLUMN_KORTEBESCHRIJVING] as? String
+        vorming.periodes = object[Constanten.COLUMN_PERIODES] as? [String]
+        vorming.prijs = object[Constanten.COLUMN_PRIJS] as? Double
+        vorming.criteriaDeelnemers = object[Constanten.COLUMN_CRITERIADEELNEMERS] as? String
+        vorming.websiteLocatie = object[Constanten.COLUMN_WEBSITELOCATIE] as? String
+        vorming.tips = object[Constanten.COLUMN_TIPS] as? String
+        vorming.betalingWijze = object[Constanten.COLUMN_BETALINGSWIJZE] as? String
+        vorming.inbegrepenPrijs = object[Constanten.COLUMN_INBEGREPENPRIJS] as? String
+        
+        return vorming
+    }
+}
