282934d5d9261c8ecb4af99b0af75973d6ce2b9f
Steefanieee <stefanie_geldof1994@hotmail.com>
2015-02-25 13:37:27 +0100

    Periodes toevoegen vakantie (1)

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/InschrijvenVakantie3ViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/InschrijvenVakantie3ViewController.swift
@@ -1,31 +1,39 @@
 import Foundation
 import UIKit
-import QuartzCore
 
-class InschrijvenVakantie3ViewController : UITableViewController {
+class InschrijvenVakantie3ViewController : UIViewController {
+    
     var inschrijvingVakantie: InschrijvingVakantie!
-    var foutBox: FoutBox? = nil
     var redColor: UIColor = UIColor.redColor()
-    var statusTextFields: [String: String] = [:]
-    var contactpersoon: ContactpersoonNood! = ContactpersoonNood(id: "test")
+    var grayColor: UIColor = UIColor.grayColor()
+    var leeftijd: Int = 0
     
-    @IBOutlet weak var txtVoornaam: UITextField!
-    @IBOutlet weak var txtNaam: UITextField!
-    @IBOutlet weak var txtTelefoon: UITextField!
-    @IBOutlet weak var txtGsm: UITextField!
+    @IBOutlet weak var dpGeboortedatum: UIDatePicker!
     
     //
-    //Naam: annuleer
+    //Naam: controleerRijksregisterNummerAlGeregistreerd
     //
-    //Werking: - zorgt ervoor wanneer de gebruiker op annuleer drukt, er een melding komt of de gebruiker zeker is van zijn beslissing
+    //Werking: - bekijkt in de databank of er al een ouder zich ingeschreven heeft met dat rijksregisternummer
     //
     //Parameters:
-    //  - sender: AnyObject
+    //
+    //Return: een bool true als het rijksregisternummer al geregistreerd is, anders false
+    //
+    @IBAction func annuleer(sender: AnyObject) {
+       annuleerControllerInschrijvenVakantieVorming(self)
+    }
+    
+    //
+    //Naam: viewDidLoad
+    //
+    //Werking:
+    //
+    //Parameters:
     //
     //Return:
     //
-    @IBAction func annuleer(sender: AnyObject) {
-        annuleerControllerInschrijvenVakantieVorming(self)
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     //
@@ -42,147 +50,53 @@ class InschrijvenVakantie3ViewController : UITableViewController {
     //
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "volgende" {
-        let inschrijvenVakantie4ViewController = segue.destinationViewController as InschrijvenVakantie4ViewController
-        
-        setStatusTextFields()
-        pasLayoutVeldenAan()
-        
-        if controleerRodeBordersAanwezig() == true {
-            foutBoxOproepen("Fout", "Gelieve de velden correct in te vullen!", self)
-        } else {
-            settenVerplichteGegevens()
+            self.leeftijd = calculateAge(dpGeboortedatum.date) as Int
+            var minLeeftijd = inschrijvingVakantie.vakantie!.minLeeftijd
+            var maxLeeftijd = inschrijvingVakantie.vakantie!.maxLeeftijd
             
-            if statusTextFields["telefoon"] != "leeg" {
-                contactpersoon.telefoon = txtTelefoon.text
+            if leeftijd < minLeeftijd || leeftijd > maxLeeftijd || leeftijd == 0 {
+                dpGeboortedatum.layer.borderColor = redColor.CGColor
+                dpGeboortedatum.layer.borderWidth = 2.0
+                dpGeboortedatum.layer.cornerRadius = 5.0
+                foutBoxOproepen("Fout", "De leeftijd moet tussen \(inschrijvingVakantie.vakantie!.minLeeftijd!) en \(inschrijvingVakantie.vakantie!.maxLeeftijd!) liggen.", self)
+                 viewDidLoad()
             }
-        
-            inschrijvingVakantie.contactpersoon1 = contactpersoon
+            
+            let inschrijvenVakantie4ViewController = segue.destinationViewController as InschrijvenVakantie4ViewController
+            inschrijvingVakantie.deelnemer?.geboortedatum = dpGeboortedatum.date
             inschrijvenVakantie4ViewController.inschrijvingVakantie = inschrijvingVakantie
-        }
+            
+            
         } else if segue.identifier == "gaTerug" {
             let vakantiesTableViewController = segue.destinationViewController as VakantiesTableViewController
         }
     }
     
     //
-    //Naam: setStatusTextFields
+    //Naam: calculateAge
     //
-    //Werking: - zet de status van de text fields in
-    //              * controleert of de velden leeg zijn
-    //              * controleert of andere validatiemethoden geldig zijn
-    //              * wanneer een text field ongeldig is krijgt deze de status "leeg" of "ongeldig"
+    //Werking: - berekent de leeftijd aan de hand van de geboortedatum
     //
     //Parameters:
+    //  - birthday: NSDate
     //
     //Return:
     //
-    func setStatusTextFields() {
-        if txtVoornaam.text.isEmpty {
-            statusTextFields["voornaam"] = "leeg"
-        } else {
-            //TO DO: checken op pattern?
-            statusTextFields["voornaam"] = "ingevuld"
-        }
+    func calculateAge (birthday: NSDate) -> NSInteger {
+        var userAge : NSInteger = 0
+        var calendar : NSCalendar = NSCalendar.currentCalendar()
+        var unitFlags : NSCalendarUnit = NSCalendarUnit.CalendarUnitYear | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitDay
+        var dateComponentNow : NSDateComponents = calendar.components(unitFlags, fromDate: NSDate())
+        var dateComponentBirth : NSDateComponents = calendar.components(unitFlags, fromDate: birthday)
         
-        if txtNaam.text.isEmpty {
-            statusTextFields["naam"] = "leeg"
-        } else {
-            //TO DO: checken op pattern?
-            statusTextFields["naam"] = "ingevuld"
+        if ( (dateComponentNow.month < dateComponentBirth.month) ||
+            ((dateComponentNow.month == dateComponentBirth.month) && (dateComponentNow.day < dateComponentBirth.day))
+            )
+        {
+            return dateComponentNow.year - dateComponentBirth.year - 1
         }
-        
-        if txtTelefoon.text.isEmpty {
-            statusTextFields["telefoon"] = "leeg"
-        } else {
-            if !checkPatternTelefoon(txtTelefoon.text) {
-                statusTextFields["telefoon"] = "ongeldig"
-            } else {
-                statusTextFields["telefoon"] = "geldig"
-            }
+        else {
+            return dateComponentNow.year - dateComponentBirth.year
         }
-        
-        if txtGsm.text.isEmpty {
-            statusTextFields["gsm"] = "leeg"
-        } else {
-            if !checkPatternGsm(txtGsm.text) {
-                statusTextFields["gsm"] = "ongeldig"
-            } else {
-                statusTextFields["gsm"] = "geldig"
-            }
-        }
-    }
-    
-    //
-    //Naam: pasLayoutVeldenAan
-    //
-    //Werking: - zorgt ervoor dat de text field, wanneer status "ongeldig" of "leeg" is, een rode border krijgt
-    //         - als deze status niet "leeg" of "ongeldig" is wordt deze border terug op default gezet
-    //
-    //Parameters:
-    //
-    //Return:
-    //
-    func pasLayoutVeldenAan() {
-        if statusTextFields["voornaam"] == "leeg" {
-            giveUITextFieldRedBorder(txtVoornaam)
-        } else {
-            giveUITextFieldDefaultBorder(txtVoornaam)
-        }
-        
-        if statusTextFields["naam"] == "leeg" {
-            giveUITextFieldRedBorder(txtNaam)
-        } else {
-            giveUITextFieldDefaultBorder(txtNaam)
-        }
-        
-        if statusTextFields["telefoon"] == "ongeldig" {
-            giveUITextFieldRedBorder(txtTelefoon)
-        } else {
-            giveUITextFieldDefaultBorder(txtTelefoon)
-        }
-        
-        if statusTextFields["gsm"] == "leeg" || statusTextFields["gsm"] == "ongeldig" {
-            giveUITextFieldRedBorder(txtGsm)
-        } else {
-            giveUITextFieldDefaultBorder(txtGsm)
-        }
-    }
-    
-    //
-    //Naam: controleerRodeBordersAanwezig
-    //
-    //Werking: - bekijkt of de text field borders een rode border hebben
-    //
-    //Parameters:
-    //
-    //Return: een bool true als er een rode border aanwezig is, anders false
-    //
-    func controleerRodeBordersAanwezig() -> Bool {
-        if CGColorEqualToColor(txtVoornaam.layer.borderColor, redColor.CGColor) {
-            return true
-        } else if CGColorEqualToColor(txtNaam.layer.borderColor, redColor.CGColor) {
-            return true
-        } else if CGColorEqualToColor(txtTelefoon.layer.borderColor, redColor.CGColor) {
-            return true
-        } else if CGColorEqualToColor(txtGsm.layer.borderColor, redColor.CGColor) {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    //
-    //Naam: settenVerplichteGegevens
-    //
-    //Werking: - afhankelijk van de status van de verplichte velden, worden de gegevens van de contactpersoon ingesteld
-    //
-    //Parameters:
-    //
-    //Return:
-    //
-    func settenVerplichteGegevens() {
-        contactpersoon.voornaam = txtVoornaam.text
-        contactpersoon.naam = txtNaam.text
-        contactpersoon.gsm = txtGsm.text
     }
 }
--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/InschrijvenVakantie5ViewController.swift
+++ iOS_huidig_goed!/GC05_iOS/GC05_iOS/InschrijvenVakantie5ViewController.swift
@@ -2,13 +2,19 @@ import Foundation
 import UIKit
 import QuartzCore
 
-class InschrijvenVakantie5ViewController : UIViewController {
-    
-    var wilExtraInfo: Bool! = true
+class InschrijvenVakantie5ViewController : UITableViewController {
+    var wilTweedeContactpersoon: Bool! = true
     var inschrijvingVakantie: InschrijvingVakantie!
-
-    @IBOutlet weak var switchExtraInfo: UISwitch!
-    @IBOutlet weak var txtViewExtraInfo: UITextView!
+    var contactpersoon2: ContactpersoonNood! = ContactpersoonNood(id: "test")
+    var statusTextFields: [String: String] = [:]
+    var redColor: UIColor = UIColor.redColor()
+    
+    @IBOutlet weak var switchTweedeContactpersoon: UISwitch!
+    @IBOutlet weak var txtVoornaam: UITextField!
+    @IBOutlet weak var txtNaam: UITextField!
+    @IBOutlet weak var txtTelefoon: UITextField!
+    @IBOutlet weak var txtGsm: UITextField!
+    @IBOutlet var labelVerplicht: UILabel!
     
     //
     //Naam: annuleer
@@ -28,8 +34,8 @@ class InschrijvenVakantie5ViewController : UIViewController {
     //Naam: switched
     //
     //Werking: - kijkt of de switch aan staat:
-    //              * switch aan: bool wilExtraInfo op true
-    //              * switch uit: bool wilExtraInfo op false
+    //              * switch aan: bool wilTweedeContactpersoon op true en toon labelVerplicht
+    //              * switch uit: bool wilTweedeContactpersoon op false en verberg labelVerplicht
     //
     //Parameters:
     //  - sender: UISwitch
@@ -38,13 +44,15 @@ class InschrijvenVakantie5ViewController : UIViewController {
     //
     @IBAction func switched(sender: UISwitch) {
         if sender.on {
-            wilExtraInfo = true
-            giveUITextViewDefaultBorder(txtViewExtraInfo)
-            txtViewExtraInfo.hidden = false
+            wilTweedeContactpersoon = true
+            labelVerplicht.hidden = false
+            viewDidLoad()
+            self.tableView.reloadData()
+            
         } else {
-            wilExtraInfo = false
-            giveUITextViewDefaultBorder(txtViewExtraInfo)
-            txtViewExtraInfo.hidden = true
+            wilTweedeContactpersoon = false
+            self.tableView.deleteSections(NSIndexSet(index: 1), withRowAnimation: UITableViewRowAnimation.None)
+            labelVerplicht.hidden = true
         }
     }
     
@@ -59,7 +67,51 @@ class InschrijvenVakantie5ViewController : UIViewController {
     //
     override func viewDidLoad() {
         super.viewDidLoad()
-        giveUITextViewDefaultBorder(txtViewExtraInfo)
+        giveUITextFieldDefaultBorder(txtVoornaam)
+        giveUITextFieldDefaultBorder(txtNaam)
+        giveUITextFieldDefaultBorder(txtTelefoon)
+        giveUITextFieldDefaultBorder(txtGsm)
+    }
+    
+    //
+    //Naam: numbersOfSectionsInTableView
+    //
+    //Werking: - zorgt dat het aantal sections zich aanpast naargelang de switch aan of uit staat
+    //
+    //Parameters:
+    //  - tableView: UITableView
+    //
+    //Return: een int met de hoeveelheid sections
+    //
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if wilTweedeContactpersoon == false {
+            return 2
+        } else {
+            return 3
+        }
+    }
+    
+    //
+    //Naam: tableView
+    //
+    //Werking: - zorgt dat het aantal rijen in een section aangepast wordt naargelang de switch aan of uit staat
+    //
+    //Parameters:
+    //  - tableView: UITableView
+    //  - numbersOfRowsInSection section: Int
+    //
+    //Return: een int met de hoeveelheid rijen per section
+    //
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if wilTweedeContactpersoon == false {
+            return 1
+        } else {
+            if section == 1 {
+                return 4
+            } else {
+                return 1
+            }
+        }
     }
     
     //
@@ -76,21 +128,148 @@ class InschrijvenVakantie5ViewController : UIViewController {
     //
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "volgende" {
-            let inschrijvenVakantieSuccesvolViewController = segue.destinationViewController as InschrijvenVakantieSuccesvolViewController
+            let inschrijvenVakantie6ViewController = segue.destinationViewController as InschrijvenVakantie6ViewController
     
-            if wilExtraInfo == true {
-                if txtViewExtraInfo.text.isEmpty {
-                    giveUITextViewRedBorder(txtViewExtraInfo)
-                    foutBoxOproepen("Fout", "Gelieve het veld in te vullen!", self)
+            if wilTweedeContactpersoon == true {
+                setStatusTextFields()
+                pasLayoutVeldenAan()
+        
+                if controleerRodeBordersAanwezig() == true {
+                    foutBoxOproepen("Fout", "Gelieve de velden correct in te vullen!", self)
                 } else {
-                    inschrijvingVakantie.extraInfo = txtViewExtraInfo.text
+                    settenVerplichteGegevens()
+                
+                    if statusTextFields["telefoon"] != "leeg" {
+                        contactpersoon2.telefoon = txtTelefoon.text
+                    }
+                    inschrijvingVakantie.contactpersoon2 = contactpersoon2
+                    inschrijvenVakantie6ViewController.inschrijvingVakantie = inschrijvingVakantie
                 }
-            } else {
-                inschrijvingVakantie.extraInfo = ""
             }
-            inschrijvenVakantieSuccesvolViewController.inschrijvingVakantie = inschrijvingVakantie
+            inschrijvenVakantie6ViewController.inschrijvingVakantie = inschrijvingVakantie
         } else if segue.identifier == "gaTerug" {
             let vakantiesTableViewController = segue.destinationViewController as VakantiesTableViewController
         }
     }
+    
+    //
+    //Naam: setStatusTextFields
+    //
+    //Werking: - zet de status van de text fields in
+    //              * controleert of de velden leeg zijn
+    //              * controleert of andere validatiemethoden geldig zijn
+    //              * wanneer een text field ongeldig is krijgt deze de status "leeg" of "ongeldig"
+    //
+    //Parameters:
+    //
+    //Return:
+    //
+    func setStatusTextFields() {
+        if txtVoornaam.text.isEmpty {
+            statusTextFields["voornaam"] = "leeg"
+        } else {
+            statusTextFields["voornaam"] = "ingevuld"
+        }
+        
+        if txtNaam.text.isEmpty {
+            statusTextFields["naam"] = "leeg"
+        } else {
+            statusTextFields["naam"] = "ingevuld"
+        }
+        
+        if txtTelefoon.text.isEmpty {
+            statusTextFields["telefoon"] = "leeg"
+        } else {
+            if !checkPatternTelefoon(txtTelefoon.text) {
+                statusTextFields["telefoon"] = "ongeldig"
+            } else {
+                statusTextFields["telefoon"] = "geldig"
+            }
+        }
+        
+        if txtGsm.text.isEmpty {
+            statusTextFields["gsm"] = "leeg"
+        } else {
+            if !checkPatternGsm(txtGsm.text) {
+                statusTextFields["gsm"] = "ongeldig"
+            } else {
+                statusTextFields["gsm"] = "geldig"
+            }
+        }
+    }
+    
+    //
+    //Naam: pasLayoutVeldenAan
+    //
+    //Werking: - zorgt ervoor dat de text field, wanneer status "ongeldig" of "leeg" is, een rode border krijgt
+    //         - als deze status niet "leeg" of "ongeldig" is wordt deze border terug op default gezet
+    //
+    //Parameters:
+    //
+    //Return:
+    //
+    func pasLayoutVeldenAan() {
+        if statusTextFields["voornaam"] == "leeg" {
+            giveUITextFieldRedBorder(txtVoornaam)
+        } else {
+            giveUITextFieldDefaultBorder(txtVoornaam)
+        }
+        
+        if statusTextFields["naam"] == "leeg" {
+            giveUITextFieldRedBorder(txtNaam)
+        } else {
+            giveUITextFieldDefaultBorder(txtNaam)
+        }
+        
+        if statusTextFields["telefoon"] == "ongeldig" {
+            giveUITextFieldRedBorder(txtTelefoon)
+        } else {
+            giveUITextFieldDefaultBorder(txtTelefoon)
+        }
+        
+        if statusTextFields["gsm"] == "leeg" || statusTextFields["gsm"] == "ongeldig" {
+            giveUITextFieldRedBorder(txtGsm)
+        } else {
+            giveUITextFieldDefaultBorder(txtGsm)
+        }
+    }
+    
+    //
+    //Naam: controleerRodeBordersAanwezig
+    //
+    //Werking: - bekijkt of de text field borders een rode border hebben
+    //
+    //Parameters:
+    //
+    //Return: een bool true als er een rode border aanwezig is, anders false
+    //
+    func controleerRodeBordersAanwezig() -> Bool {
+        if CGColorEqualToColor(txtVoornaam.layer.borderColor, redColor.CGColor) {
+            return true
+        } else if CGColorEqualToColor(txtNaam.layer.borderColor, redColor.CGColor) {
+            return true
+        } else if CGColorEqualToColor(txtTelefoon.layer.borderColor, redColor.CGColor) {
+            return true
+        } else if CGColorEqualToColor(txtGsm.layer.borderColor, redColor.CGColor) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    //
+    //Naam: settenVerplichteGegevens
+    //
+    //Werking: - afhankelijk van de status van de verplichte velden, worden de gegevens van de contactpersoon2 ingesteld
+    //
+    //Parameters:
+    //
+    //Return:
+    //
+    func settenVerplichteGegevens() {
+        contactpersoon2.voornaam = txtVoornaam.text
+        contactpersoon2.naam = txtNaam.text
+        contactpersoon2.gsm = txtGsm.text
+    }
+    
 }
