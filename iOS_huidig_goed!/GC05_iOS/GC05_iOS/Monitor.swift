4b20771e412b40de03a9eee0ac246f39aadb70c0
CharlotteErpels1993 <charlotte.erpels@hotmail.com>
2015-02-25 13:54:39 +0100

    test

--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/Monitor.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class Monitor: Gebruiker {
-    var lidNr: String?
-    
-    override init(id: String) {
-        super.init(id: id)
-    }
-}
--- iOS_huidig_goed!/GC05_iOS/GC05_iOS/Ouder.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class Ouder: Gebruiker {
-    var aansluitingsNrTweedeOuder: Int?
-    
-    override init(id: String) {
-        super.init(id: id)
-    }
-}
