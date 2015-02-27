05a82664b518c570caed6a3f90a1ff6bf6ded6ae
GillesDV <gilles.devylder@hotmail.com>
2015-02-27 11:11:52 +0100

    default info vervangen + Android heeft dingen veranderd

--- Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/SignUpLogin/RegistrerenDeel4.java
+++ Android/TI3_G05_JoetzApp2/app/src/main/java/com/hogent/ti3g05/ti3_g05_joetzapp/SignUpLogin/RegistrerenDeel4.java
@@ -83,7 +83,7 @@ public class RegistrerenDeel4 extends Activity{
         tvOpvullen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mEmailEditText.setText("albert.bosman@gmail.com");
+                mEmailEditText.setText("gilles.devylder@gmail.com");
                 mPasswordEditText.setText("abc123");
                 mConfirmPasswordEditText.setText("abc123");
 
